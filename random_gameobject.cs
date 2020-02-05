45aa2271988685b0fb52d2760b9159e47b6a3195
Tanat29 <tanat29.mongkon@gmail.com>
2020-02-05 08:29:22 +0700

    Add files via upload

---
+++ random_gameobject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class random_gameobject : MonoBehaviour
+{
+    public GameObject gameobject1,gameobject2,gameobject3,gameobject4,gameobject5;
+
+    public void Random1234() {
+        GameObject[] gb = {gameobject1,gameobject2,gameobject3,gameobject4,gameobject5};
+
+        for(int i=0;i<gb.Length;i++) {
+            if(gb[i].activeSelf == true) {
+                gb[i].SetActive(false);
+            }
+        }
+
+        int newIndex = Random.RandomRange(0,gb.Length);
+        gb[newIndex].SetActive(true);
+    }
+}
